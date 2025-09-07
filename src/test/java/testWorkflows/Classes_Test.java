5ab96c0fdc6a936c672030d2401007758a3fedd6
Mai Okasha <94134290+MaiOkasha@users.noreply.github.com>
2025-09-08 02:30:20 +0300

    Update Classes_Test.java

--- src/test/java/testWorkflows/Classes_Test.java
+++ src/test/java/testWorkflows/Classes_Test.java
@@ -8,8 +8,6 @@ import org.openqa.selenium.WebElement;
 import dev.failsafe.internal.util.Assert;
 import pages.ClassesPage;
 
-//import Automation_Project.Employees_Section;
-
 public class Classes_Test extends BaseTest {
 
 	ClassesPage test1;
@@ -25,7 +23,6 @@ public class Classes_Test extends BaseTest {
 
 		test1.login("badaha.22@gmail.com", "Aa00147235@@");
 		test1.allclasses();
-		// test1.Edit_class();
 		test1.delete_class();
 	}
 
@@ -33,7 +30,6 @@ public class Classes_Test extends BaseTest {
 	public void add_class(String classn, String fees, String teacher) throws InterruptedException {
 
 		test1.addnew_Class(classn, fees, teacher);
-		// Assert that the new element shows up on the page
 		WebElement newClass = driver.findElement(By.xpath("//h6[contains(text(),'Grade 1')]"));
 		Assert.notNull(newClass, "New class was not found after adding.");
 		System.out.println(newClass);
